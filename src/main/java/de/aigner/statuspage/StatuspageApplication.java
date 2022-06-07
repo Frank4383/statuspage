aa5a0cb4090b3d2a50417c78d22f698f0b6662a7
Frank Aigner <frank_aigner@hotmail.com>
2022-06-07 20:23:59 +0200

    init

---
+++ src/main/java/de/aigner/statuspage/StatuspageApplication.java
@@ -0,0 +1,13 @@
+package de.aigner.statuspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatuspageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatuspageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/aigner/statuspage/StatuspageApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aigner.statuspage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatuspageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
