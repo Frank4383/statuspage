f0b8f67eb2c7450e120f676167aa2bec9641cf3f
Frank Aigner <frank_aigner@hotmail.com>
2022-06-07 22:04:02 +0200

    init

--- src/main/java/de/aigner/statuspage/StatuspageApplication.java
+++ src/main/java/de/aigner/statuspage/StatuspageApplication.java
@@ -2,12 +2,21 @@ package de.aigner.statuspage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StatuspageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StatuspageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StatuspageApplication.class, args);
+    }
+
+    @GetMapping("/admin")
+    public String adminSeite(@RequestParam(value = "meinName", defaultValue = "Admin") String name) {
+        return String.format("Hallo %s!", name);
+    }
 
 }
